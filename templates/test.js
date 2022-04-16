86b607c4b384be9d9a2930c714728ad3d88ba4d7
TravetskaN <ntravetskaya@gmail.com>
2022-04-16 21:03:08 +0100

    дз

---
+++ templates/test.js
@@ -0,0 +1,10 @@
+// const [{name,capital,population,flags,languages,}] = countryUser;
+// const markup = countryUser.map(allCountries).join("");
+// countryList.innerHTML = markup;
+// const values = Object.values(languages);
+
+// let a = 5;
+// let b = "Hello";
+
+// [a,b] = [b,a];
+// console.log(a, b)
